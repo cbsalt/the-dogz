6eeddad5993463b39fc51a71874cd4900335ae56
CHARLES SAL <cbsal.tec@gmail.com>
2021-02-14 15:11:04 -0300

    feat: reset global css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
-    <div>App React</div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
