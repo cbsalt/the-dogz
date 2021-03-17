b0543ee6f5711a2568cd8126aab0196375947555
CHARLES SAL <cbsal.tec@gmail.com>
2021-03-17 20:25:43 -0300

    feat: infinite scroll

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+import { UserContext } from '../../UserContext';
 import Feed from '../Feed/Feed';
 import UserHeader from './UserHeader';
 import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
 
-const User = () => (
-  <section className="container">
-    <UserHeader />
-    <Routes>
-      <Route path="/" element={<Feed />} />
-      <Route path="/post" element={<UserPhotoPost />} />
-      <Route path="/statistics" element={<UserStats />} />
-    </Routes>
-  </section>
-);
+const User = () => {
+  const { data } = useContext(UserContext);
+
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed user={data.id} />} />
+        <Route path="/post" element={<UserPhotoPost />} />
+        <Route path="/statistics" element={<UserStats />} />
+      </Routes>
+    </section>
+  );
+};
 
 export default User;
