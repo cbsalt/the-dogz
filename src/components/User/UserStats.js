d9428e2f0cc7bb81a30c8df7eede1f1b623f729f
CHARLES SAL <cbsal.tec@gmail.com>
2021-03-19 13:58:38 -0300

    feat: graph stats

--- src/components/User/UserStats.js
+++ src/components/User/UserStats.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import Head from '../Helper/Head';
+import React, { useEffect } from 'react';
 
-const UserStats = () => (
-  <>
-    <Head title="Estatísticas" />
-    <div>
-      Estatísticas
-    </div>
-  </>
-);
+import Head from '../Helper/Head';
+import Loader from '../Helper/Loader';
+import Error from '../Helper/Error';
+
+import useFetch from '../../hooks/useFetch';
+import { STATS_GET } from '../../api';
+
+import UserStatsGraphs from './UserStatsGraphs';
+
+const UserStats = () => {
+  const {
+    data, error, request, loading,
+  } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loader />;
+  if (error) return <Error error={error} />;
+  if (data) {
+    return (
+      <>
+        <Head title="Estatísticas" />
+        <div>
+          <UserStatsGraphs data={data} />
+        </div>
+      </>
+    );
+  }
+  return null;
+};
 
 export default UserStats;
